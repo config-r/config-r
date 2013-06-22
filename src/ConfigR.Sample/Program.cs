7f829d0c412dea838a8ad58d497d520df30436c4
Adam Ralph <adam@adamralph.com>
2013-06-22 12:24:01 +0200

    improved sample

--- src/ConfigR.Sample/Program.cs
+++ src/ConfigR.Sample/Program.cs
@@ -13,11 +13,19 @@ namespace ConfigR.Sample
         [SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "System.Console.WriteLine(System.String,System.Object,System.Object)", Justification = "Just a sample.")]
         public static void Main(string[] args)
         {
+            // you can retreive settings as dynamics
+            var dynamicCount = Configurator.Get("Count");
+            var dynamicUri = Configurator.Get("Uri");
+
+            Console.WriteLine("Count: ({0}) {1}", dynamicCount.GetType(), dynamicCount);
+            Console.WriteLine("Uri: ({0}) {1}", dynamicUri.GetType(), dynamicUri);
+
+            // you can also retreive settings as their underlying type
             var count = Configurator.Get<int>("Count");
             var uri = Configurator.Get<Uri>("Uri");
 
-            Console.WriteLine("Count: ({0}) {1}", count.GetType(), count);
-            Console.WriteLine("Uri: ({0}) {1}", uri.GetType(), uri);
+            Console.WriteLine("Count * 2: {0}", count * 2);
+            Console.WriteLine("Uri.AbsolutePath: {0}", uri.AbsolutePath);
 
             Console.WriteLine("Brutalize a key with your favourite finger to exit.");
             Console.ReadKey();
