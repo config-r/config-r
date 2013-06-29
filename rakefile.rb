31851b97d3f32a0eaaf8275681d8175e6fdfd27c
Adam Ralph <adam@adamralph.com>
2013-06-30 01:17:17 +0200

    #30 removed xunit noshadow switch - redundant after proper file system impl

--- rakefile.rb
+++ rakefile.rb
@@ -74,7 +74,7 @@ def execute_xunit(tests)
     xunit = XUnitTestRunner.new
     xunit.command = test[:command]
     xunit.assembly = test[:assembly]
-    xunit.options "/html", File.expand_path(test[:assembly] + ".TestResults.html"), "/xml", File.expand_path(test[:assembly] + ".TestResults.xml"), "/noshadow"
+    xunit.options "/html", File.expand_path(test[:assembly] + ".TestResults.html"), "/xml", File.expand_path(test[:assembly] + ".TestResults.xml")
     xunit.execute  
   end
 end
