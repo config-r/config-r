b06d008e7217ed871fb1afb7105cd282b03af9a2
Adam Ralph <adam@adamralph.com>
2013-06-30 20:08:12 +0200

    #24 added testing website

---
+++ src/test/ConfigR.Testing.Website/IndexModule.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="IndexModule.cs" company="ConfigR contributors">
+//  Copyright (c) ConfigR contributors. (configr.net@gmail.com)
+// </copyright>
+
+namespace ConfigR.Testing.Website
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            this.Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
