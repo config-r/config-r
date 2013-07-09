9be1c325fa38d08bb09b2b46c5a85e3f2ccd523a
Adam Ralph <adam@adamralph.com>
2013-07-09 08:42:47 +0200

    #8 moved ScriptExecutor wrappings from FileConfigurator to ConfigRScriptExecutor

--- src/ConfigR/FileConfigurator.cs
+++ src/ConfigR/FileConfigurator.cs
@@ -4,15 +4,15 @@
 
 namespace ConfigR
 {
-    using System;
     using System.Globalization;
     using Common.Logging;
     using ScriptCs;
-    using ScriptCs.Engine.Roslyn;
 
     public class FileConfigurator : BasicConfigurator
     {
         private static readonly ILog log = LogManager.GetCurrentClassLogger();
+
+        private readonly IFileSystem fileSystem = new ConfigRFileSystem(new FileSystem());
         private readonly string path;
 
         public FileConfigurator(string path)
@@ -27,35 +27,13 @@ namespace ConfigR
 
         public override IConfigurator Load()
         {
-            var fileSystem = new ConfigRFileSystem(new FileSystem());
-            log.DebugFormat(CultureInfo.InvariantCulture, "Initialized file system with current directory {0}", fileSystem.CurrentDirectory);
+            log.DebugFormat(CultureInfo.InvariantCulture, "The current directory is {0}", this.fileSystem.CurrentDirectory);
+            log.InfoFormat(CultureInfo.InvariantCulture, "Loading '{0}'", this.fileSystem.GetFullPath(this.path));
 
-            log.InfoFormat(CultureInfo.InvariantCulture, "Loading '{0}'", fileSystem.GetFullPath(this.path));
-
-            var scriptCsLog = LogManager.GetLogger("ScriptCs");
-            var engine = new RoslynScriptEngine(new ScriptHostFactory(), scriptCsLog);
-            var executor = new ScriptExecutor(fileSystem, new FilePreProcessor(fileSystem, scriptCsLog), engine, scriptCsLog);
-
-            log.DebugFormat(CultureInfo.InvariantCulture, "Initializing script executor");
-            executor.Initialize(new string[0], new[] { new ConfigRScriptHack() });
-            engine.BaseDirectory = fileSystem.CurrentDirectory; // NOTE (adamralph): set to bin subfolder in executor.Initialize()!
-
-            log.DebugFormat(CultureInfo.InvariantCulture, "Compiling and executing configuration script {0}", this.path);
-            var result = executor.Execute(this.path);
-
-            log.DebugFormat(CultureInfo.InvariantCulture, "Terminating script executor");
-            executor.Terminate();
-
-            if (result.CompileException != null)
+            using (var executor = new ConfigRScriptExecutor(this.fileSystem))
             {
-                log.ErrorFormat(CultureInfo.InvariantCulture, "Error compiling configuration script {0}", result.CompileException, this.path);
-                throw new InvalidOperationException("Failed to compile " + this.path, result.CompileException);
-            }
-
-            if (result.ExecuteException != null)
-            {
-                log.ErrorFormat(CultureInfo.InvariantCulture, "Error executing configuration script {0}", result.ExecuteException, this.path);
-                throw new InvalidOperationException("Failed to execute " + this.path, result.ExecuteException);
+                executor.Initialize(new string[0], new[] { new ConfigRScriptHack() });
+                executor.Execute(this.path);
             }
 
             return this;
