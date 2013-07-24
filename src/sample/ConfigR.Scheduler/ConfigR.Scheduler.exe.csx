03c3504823d20227a2ac1f6b3b03cbdc04f749c1
Adam Ralph <adam@adamralph.com>
2013-07-24 17:47:31 +0200

    added scheduler sample

---
+++ src/sample/ConfigR.Scheduler/ConfigR.Scheduler.exe.csx
@@ -0,0 +1,29 @@
+﻿#r "ConfigR.Scheduler.exe"
+
+using System;
+using ConfigR.Scheduler;
+
+Configurator
+    .Add(
+        "Schedules",
+        new[]
+        {
+            new Schedule
+            {
+                Action = () => Console.WriteLine("First schedule is running!"),
+                NextRun = DateTime.Now.AddSeconds(1),
+                RepeatInterval = TimeSpan.FromSeconds(4),
+            },
+            new Schedule
+            {
+                Action = () => Console.WriteLine("Second schedule is running!"),
+                NextRun = DateTime.Now.AddSeconds(2),
+                RepeatInterval = TimeSpan.FromSeconds(4),
+            },
+            new Schedule
+            {
+                Action = () => Console.WriteLine("Third schedule is running!"),
+                NextRun = DateTime.Now.AddSeconds(2),
+                RepeatInterval = TimeSpan.FromSeconds(4),
+            },
+        });
---
+++ src/sample/ConfigR.Scheduler/Program.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="Program.cs" company="ConfigR contributors">
+//  Copyright (c) ConfigR contributors. (configr.net@gmail.com)
+// </copyright>
+
+namespace ConfigR.Scheduler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using Common.Logging;
+    using ConfigR;
+    using Topshelf;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var log = LogManager.GetCurrentClassLogger();
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) => log.Error((Exception)e.ExceptionObject);
+            HostFactory.Run(h => h.Service<Schedule[]>(s =>
+            {
+                s.ConstructUsing(() => Configurator.Get<Schedule[]>("Schedules"));
+
+                Dictionary<Schedule, Timer> timers = null;
+                s.WhenStarted(schedules => timers = schedules.ToDictionary(
+                    schedule => schedule,
+                    schedule => new Timer(
+                        state =>
+                        {
+                            try
+                            {
+                                schedule.Action();
+                            }
+                            catch (Exception ex)
+                            {
+                                log.Error("Error executing schedule", ex);
+                            }
+
+                            timers[schedule].Change((schedule.NextRun += schedule.RepeatInterval) - DateTime.Now, TimeSpan.FromMilliseconds(-1));
+                        },
+                        null,
+                        schedule.NextRun - DateTime.Now,
+                        TimeSpan.FromMilliseconds(-1))));
+
+                s.WhenStopped(schedules => timers.Values.ToList().ForEach(timer => timer.Dispose()));
+            }));
+        }
+    }
+}
