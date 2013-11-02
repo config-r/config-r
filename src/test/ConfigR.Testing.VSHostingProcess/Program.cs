f84055b964f2d48965e041f0769734e685cdc675
Adam Ralph <adam@adamralph.com>
2013-11-02 12:28:33 +0100

    #86 added support for the Visual Studio hosting process

---
+++ src/test/ConfigR.Testing.VSHostingProcess/Program.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Program.cs" company="ConfigR contributors">
+//  Copyright (c) ConfigR contributors. (configr.net@gmail.com)
+// </copyright>
+
+namespace ConfigR.Testing.VSHostingProcess
+{
+    using System;
+    using Common.Logging;
+    using Common.Logging.Simple;
+    using ConfigR;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            LogManager.Adapter = new ConsoleOutLoggerFactoryAdapter(LogLevel.Trace, false, true, true, null);
+
+            Console.WriteLine(Configurator.Get<string>("greeting"));
+
+            Console.WriteLine("Brutalize a key with your favourite finger to exit.");
+            Console.ReadKey();
+        }
+    }
+}
