ad30a063ca26bf6d4546d7e3e6d14eca559877cd
Adam Ralph <adam@adamralph.com>
2014-01-10 12:22:47 +0100

    #148 red: added tests for config files containing only comments

---
+++ src/test/ConfigR.Features/EmptyConfigFeature.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="EmptyConfigFeature.cs" company="ConfigR contributors">
+//  Copyright (c) ConfigR contributors. (configr.net@gmail.com)
+// </copyright>
+
+namespace ConfigR.Features
+{
+    using System.IO;
+    using FluentAssertions;
+    using Xbehave;
+    using Xunit;
+
+    public static class EmptyConfigFeature
+    {
+        [Background]
+        public static void Background()
+        {
+            "Given no configuration has been loaded"
+                .Given(() => Config.Global.Reset());
+        }
+
+        [Scenario]
+        [Example(null)]
+        [Example("")]
+        [Example(" ")]
+        [Example("\n")]
+        [Example("//")]
+        [Example("// a comment")]
+        public static void EmptyConfig(string code, object exception)
+        {
+            "Given a config file which contains no executable code"
+                .Given(() =>
+                {
+                    using (var writer = new StreamWriter(LocalScriptFileConfig.Path))
+                    {
+                        writer.WriteLine(code);
+                        writer.Flush();
+                    }
+                })
+                .Teardown(() => File.Delete(LocalScriptFileConfig.Path));
+
+            "When I load the config"
+                .When(() => exception = Record.Exception(() => Config.Global));
+
+            "Then no exception is thrown"
+                .Then(() => exception.Should().BeNull());
+        }
+    }
+}
