6cc129cdf02dd7784b226489d4af3b5b4f74339a
Adam Ralph <adam@adamralph.com>
2014-01-10 11:12:06 +0100

    #148 refactor: move throwing from ConfigRScriptExecutor to ScriptConfigLoader

--- src/ConfigR/Scripting/ConfigRScriptExecutor.cs
+++ src/ConfigR/Scripting/ConfigRScriptExecutor.cs
@@ -6,7 +6,6 @@ namespace ConfigR.Scripting
 {
     using System;
     using System.Collections.Generic;
-    using System.Globalization;
     using Common.Logging;
     using ScriptCs;
     using ScriptCs.Contracts;
@@ -34,41 +33,12 @@ namespace ConfigR.Scripting
             this.isInitialized = true;
         }
 
-        public override ScriptResult ExecuteScript(string script, params string[] scriptArgs)
-        {
-            var result = base.ExecuteScript(script, scriptArgs);
-            RethrowExceptionIfAny(result, script);
-            return result;
-        }
-
-        public override ScriptResult Execute(string script, string[] scriptArgs)
-        {
-            var result = base.Execute(script, scriptArgs);
-            RethrowExceptionIfAny(result, script);
-            return result;
-        }
-
         public void Dispose()
         {
             if (this.isInitialized)
             {
                 this.Terminate();
                 this.isInitialized = false;
-            }
-        }
-
-        private static void RethrowExceptionIfAny(ScriptResult result, string script)
-        {
-            if (result.CompileExceptionInfo != null)
-            {
-                log.ErrorFormat(CultureInfo.InvariantCulture, "Failed to compile {0}", result.CompileExceptionInfo, script);
-                result.CompileExceptionInfo.Throw();
-            }
-
-            if (result.ExecuteExceptionInfo != null)
-            {
-                log.ErrorFormat(CultureInfo.InvariantCulture, "Failed to execute {0}", result.ExecuteExceptionInfo, script);
-                result.ExecuteExceptionInfo.Throw();
             }
         }
     }
--- src/ConfigR/Scripting/ScriptConfigLoader.cs
+++ src/ConfigR/Scripting/ScriptConfigLoader.cs
@@ -21,11 +21,31 @@ namespace ConfigR.Scripting
         {
             log.InfoFormat(CultureInfo.InvariantCulture, "Executing '{0}'", this.fileSystem.GetFullPath(path));
             log.DebugFormat(CultureInfo.InvariantCulture, "The current directory is {0}", this.fileSystem.CurrentDirectory);
+            
+            ScriptResult result;
             using (var executor = new ConfigRScriptExecutor(config, this.fileSystem))
             {
                 executor.AddReferenceAndImportNamespaces(new[] { typeof(Config) });
                 executor.Initialize(new string[0], new IScriptPack[0]);
-                return executor.Execute(path).ReturnValue;
+                result= executor.Execute(path);
+            }
+
+            RethrowExceptionIfAny(result, path);
+            return result.ReturnValue;
+        }
+
+        private static void RethrowExceptionIfAny(ScriptResult result, string scriptPath)
+        {
+            if (result.CompileExceptionInfo != null)
+            {
+                log.ErrorFormat(CultureInfo.InvariantCulture, "Failed to compile {0}", result.CompileExceptionInfo, scriptPath);
+                result.CompileExceptionInfo.Throw();
+            }
+
+            if (result.ExecuteExceptionInfo != null)
+            {
+                log.ErrorFormat(CultureInfo.InvariantCulture, "Failed to execute {0}", result.ExecuteExceptionInfo, scriptPath);
+                result.ExecuteExceptionInfo.Throw();
             }
         }
     }
