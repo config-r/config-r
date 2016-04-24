a723e95ed20ee21d524f491b9efe3c0b3a8edb12
Adam Ralph <adam@adamralph.com>
2016-04-25 00:56:46 +0200

    refactor: changes during switch from scriptcs to roslyn  - inline single use variables  - remove redundant comment inherited from website sample  - sort default usings in host

--- tests/ConfigR.Testing.Website/Global.asax.cs
+++ tests/ConfigR.Testing.Website/Global.asax.cs
@@ -12,11 +12,6 @@ namespace ConfigR.Samples.WebApplication
     {
         protected void Application_Start(object sender, EventArgs e)
         {
-            // NOTE (adamralph): If you want to load a specific config, e.g. Web.Debug.csx or Web.Release.csx, and you also want Web.csx to be loaded,
-            // you need to call LoadLocal() before calling Load("...") otherwise ConfigR will assume that you *only* want to load the specific config.
-            // On the other hand, if you are *not* loading any specific config and you only want Web.csx to be loaded,
-            // you don't need to call any Load...() methods, ConfigR will do the loading for you as usual.
-            // In that case, this entire class could be removed from this sample.
             Config.Global
                 .LoadLocalScriptFile()
 #if DEBUG
