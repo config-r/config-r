f36df4acdce7a0f754075ca95d207e64942d3be2
Filip W <wojcieszyn@gmail.com>
2016-05-11 21:03:05 +0200

    MetadataReference and in memory assemblies support (#244)
    * added support for MetadataReference and in memory assemblies
    
    * clean up code

--- src/ConfigR/ScriptConfig.cs
+++ src/ConfigR/ScriptConfig.cs
@@ -2,6 +2,8 @@
 //  Copyright (c) ConfigR contributors. (configr.net@gmail.com)
 // </copyright>
 
+using Microsoft.CodeAnalysis;
+
 namespace ConfigR
 {
     using System.Diagnostics.CodeAnalysis;
@@ -12,10 +14,19 @@ namespace ConfigR
     public abstract class ScriptConfig : BasicConfig
     {
         private readonly Assembly[] references;
+        private MetadataReference[] metadataReferences;
 
         protected ScriptConfig(params Assembly[] references)
         {
             this.references = (references ?? Enumerable.Empty<Assembly>()).ToArray();
+        }
+
+#pragma warning disable CS3001
+        protected ScriptConfig(Assembly[] references, MetadataReference[] metadataReferences)
+#pragma warning restore CS3001
+        {
+            this.references = (references ?? Enumerable.Empty<Assembly>()).ToArray();
+            this.metadataReferences = (metadataReferences ?? Enumerable.Empty<MetadataReference>()).ToArray();
         }
 
         public override ISimpleConfig Load()
@@ -26,7 +37,7 @@ namespace ConfigR
 
         protected virtual void Load(string scriptPath)
         {
-            new ScriptConfigLoader(this.references.ToArray()).LoadFromFile(this, scriptPath);
+            new ScriptConfigLoader(this.references, this.metadataReferences).LoadFromFile(this, scriptPath);
         }
 
         [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Not appropriate.")]
--- src/ConfigR/ScriptFileConfig.cs
+++ src/ConfigR/ScriptFileConfig.cs
@@ -2,6 +2,8 @@
 //  Copyright (c) ConfigR contributors. (configr.net@gmail.com)
 // </copyright>
 
+using Microsoft.CodeAnalysis;
+
 namespace ConfigR
 {
     using System.Reflection;
@@ -12,6 +14,14 @@ namespace ConfigR
 
         public ScriptFileConfig(string path, params Assembly[] references)
             : base(references)
+        {
+            this.path = path;
+        }
+
+#pragma warning disable CS3001
+        public ScriptFileConfig(string path, Assembly[] references, MetadataReference[] metadataReferences)
+#pragma warning restore CS3001
+            : base(references, metadataReferences)
         {
             this.path = path;
         }
