e01de4401dca5b779d6f5313faec69863aad2f45
Adam Ralph <adam@adamralph.com>
2016-05-21 18:51:03 +0200

    green: added scenarios to SeedingFeature  - SeedingAScriptWithAnObjectAndReceivingADictionary  - SeedingAScriptWithADictionaryAndReceivingADictionary

--- tests/ConfigR.Tests.Acceptance.Roslyn.CSharp/SeedingFeature.cs
+++ tests/ConfigR.Tests.Acceptance.Roslyn.CSharp/SeedingFeature.cs
@@ -46,5 +46,41 @@ namespace ConfigR.Tests.Acceptance.Roslyn.CSharp
             "Then Foo is 'baz'"
                 .f(() => result.Should().Be("baz"));
         }
+
+        [Scenario]
+        public static void SeedingAScriptWithAnObjectAndReceivingADictionary(string result)
+        {
+            IDictionary<string, object> config = null;
+
+            "Given a local config file which sets Foo using the value of Bar"
+                .f(c => ConfigFile.Create(@"Config.Foo = Config.Bar;").Using(c));
+
+            "And I load the config seeded with Bar set to 'baz'"
+                .f(async () => config = await new Config().UseRoslynCSharpLoader().LoadDictionary(new { Bar = "baz" }));
+
+            "And I get Foo"
+                .f(() => result = config.Get<string>("Foo"));
+
+            "Then Foo is 'baz'"
+                .f(() => result.Should().Be("baz"));
+        }
+
+        [Scenario]
+        public static void SeedingAScriptWithADictionaryAndReceivingADictionary(string result)
+        {
+            IDictionary<string, object> config = null;
+
+            "Given a local config file which sets Foo using the value of Bar"
+                .f(c => ConfigFile.Create(@"Config.Foo = Config.Bar;").Using(c));
+
+            "And I load the config seeded with Bar set to 'baz'"
+                .f(async () => config = await new Config().UseRoslynCSharpLoader().LoadDictionary(new Dictionary<string, object> { { "Bar", "baz" } }));
+
+            "And I get Foo"
+                .f(() => result = config.Get<string>("Foo"));
+
+            "Then Foo is 'baz'"
+                .f(() => result.Should().Be("baz"));
+        }
     }
 }
