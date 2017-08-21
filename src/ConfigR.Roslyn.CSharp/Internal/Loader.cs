0a6fac5f56d8c0ab53d44185cdb35afc15828351
filipw <wojcieszyn@gmail.com>
2017-08-21 14:19:11 +0200

    C# 7.1 - green

--- src/ConfigR.Roslyn.CSharp/Internal/Loader.cs
+++ src/ConfigR.Roslyn.CSharp/Internal/Loader.cs
@@ -12,6 +12,9 @@ namespace ConfigR.Roslyn.CSharp.Internal
     using Microsoft.CodeAnalysis.CSharp.Scripting;
     using Microsoft.CodeAnalysis.Scripting;
     using Microsoft.CodeAnalysis.Scripting.Hosting;
+    using System.Reflection;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis;
 
     public class Loader : ILoader
     {
@@ -27,6 +30,7 @@ namespace ConfigR.Roslyn.CSharp.Internal
             this.scriptUri = scriptUri;
             this.options = options;
             this.assemblyLoader = assemblyLoader;
+            SetCSharpVersionToLatest();
         }
 
         public async Task<DynamicDictionary> Load(DynamicDictionary config)
@@ -39,6 +43,22 @@ namespace ConfigR.Roslyn.CSharp.Internal
                 .RunAsync(new ScriptGlobals(config));
 
             return config;
+        }
+
+        private void SetCSharpVersionToLatest()
+        {
+            try
+            {
+                // reset default scripting mode to latest language version to enable C# 7.1 features
+                // this is not needed once https://github.com/dotnet/roslyn/pull/21331 ships
+                var csharpScriptCompilerType = typeof(CSharpScript).GetTypeInfo().Assembly.GetType("Microsoft.CodeAnalysis.CSharp.Scripting.CSharpScriptCompiler");
+                var parseOptionsField = csharpScriptCompilerType?.GetField("s_defaultOptions", BindingFlags.Static | BindingFlags.NonPublic);
+                parseOptionsField?.SetValue(null, new CSharpParseOptions(LanguageVersion.Latest, kind: SourceCodeKind.Script));
+            }
+            catch (Exception)
+            {
+                log.Warn("Unable to set C# language version to latest, will use the default version.");
+            }
         }
     }
 }
